8c3f0d3e352062a078f9ac2a7962a44a7d490379
Harsh Sharma <134474399+hey-hs@users.noreply.github.com>
2024-08-02 17:51:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get("/api/users", async (_req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' () => {
   console.log(`Server is running on port ${PORT}`);
 });
